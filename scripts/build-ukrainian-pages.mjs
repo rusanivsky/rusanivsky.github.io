f8a9e0532af5bcefb4df33c962e8952061d833a5
Kyrylo Rusanivsky <90484735+rusanivsky@users.noreply.github.com>
2026-09-11 20:16:20 +0300

    Match section scrolling with home page

--- scripts/build-ukrainian-pages.mjs
+++ scripts/build-ukrainian-pages.mjs
@@ -111,7 +111,7 @@ function addContactsLink(html, route) {
   return html;
 }
 
-const sectionHeaderStyleVersion = 9;
+const sectionHeaderStyleVersion = 10;
 
 function addSectionHeaderStyle(html, route) {
   if (route === '/') return html;
